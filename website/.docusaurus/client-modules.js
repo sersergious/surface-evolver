fd9df324f5feaa6647511961dbd6eb1270ff57dc
Serhii Kuzmin <sergeykuzmin495@gmail.com>
2026-04-21 22:24:27 -0400

    Remove Docusaurus docs site [skip ci]

--- website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/sersergious/dev-projects/surface-evolver/website/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/sersergious/dev-projects/surface-evolver/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/sersergious/dev-projects/surface-evolver/website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/sersergious/dev-projects/surface-evolver/website/src/css/custom.css"),
-];
